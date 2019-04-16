5e1af6bfa09d052e5bcbeb8afd76d10aa8251b63
BokiGaja <bgajic93@gmail.com>
2019-04-16 10:10:05 +0200

    AuthService added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\AuthService;
 use App\Http\Services\ValidationService;
 use App\User;
 use Illuminate\Http\Request;
@@ -18,13 +19,7 @@ class AuthController extends Controller
         $validator = ValidationService::validateUser($request);
         if (!is_string($validator))
         {
-            $newUser = new User();
-            $newUser->first_name = $request->first_name;
-            $newUser->last_name = $request->last_name;
-            $newUser->password = bcrypt($request->password);
-            $newUser->email = $request->email;
-            $newUser->save();
-            auth()->login($newUser);
+            AuthService::registerUser($request);
         } else {
             return response()->json(['error' => $validator]);
         }
@@ -32,31 +27,11 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $credentials = $request->only(['email', 'password']);
-        if (! $token = auth('api')->attempt($credentials)) {
-            return response()->json(['error' => 'Wrong credentials']);
-        }
-        return $this->respondWithToken($token);
+        return AuthService::loginUser($request);
     }
 
     public function logout()
     {
-        auth('api')->logout();
-        return "Successfully logged out";
-    }
-
-    public function respondWithToken($token)
-    {
-        return response()->json([
-            'acces_token' => $token,
-            'user' => $this->guard()->user(),
-            'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
-        ]);
-    }
-
-    public function guard()
-    {
-        return \Auth::Guard('api');
+        AuthService::logoutUser();
     }
 }
---
+++ app/Http/Services/AuthService.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Http\Services;
+
+use App\User;
+
+class AuthService
+{
+    public static function registerUser($userData)
+    {
+        $newUser = new User();
+        $newUser->first_name = $userData->first_name;
+        $newUser->last_name = $userData->last_name;
+        $newUser->password = bcrypt($userData->password);
+        $newUser->email = $userData->email;
+        $newUser->save();
+        auth()->login($newUser);
+    }
+
+    public static function loginUser($userData)
+    {
+        $credentials = $userData->only(['email', 'password']);
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Wrong credentials']);
+        }
+        return AuthService::respondWithToken($token);
+    }
+
+    public static function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'user' => AuthService::guard()->user(),
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public static function guard()
+    {
+        return \Auth::Guard('api');
+    }
+
+    public static function logoutUser()
+    {
+        auth('api')->logout();
+        return "Successfully logged out";
+    }
+}
